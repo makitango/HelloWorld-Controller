73189f32dbc351170d74253bdf605ba77a490fe8
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-13 10:51:24 +0200

    initial commit

---
+++ src/main/java/com/example/helloworldcontroller/HelloWorldControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldcontroller/HelloWorldControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
